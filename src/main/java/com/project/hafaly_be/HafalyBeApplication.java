58ba8a5fc00ba5fe4e205d159a23e1274c29f1e9
duclongdev <20521566@gm.uit.edu.vn>
2022-12-31 00:04:55 +0700

    first commit

---
+++ src/main/java/com/project/hafaly_be/HafalyBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.hafaly_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafalyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HafalyBeApplication.class, args);
+    }
+
+}
